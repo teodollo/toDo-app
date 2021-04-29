adceb8890469ef64ab5febfd47b17cae7529524d
Teodoro Monteiro <58260478+teodollo@users.noreply.github.com>
2021-04-29 22:36:13 +0200

    created adding of task to list with input

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+const todoInput = document.getElementById("todo-input");
 const input = document.getElementById("input");
 const list = document.getElementById("todo-list");
 const todo = document.getElementsByClassName("todo");
 
-//li button
-console.log(list.children[0].children[0])
 
 list.addEventListener('click', (e) => {
   if (e.target.className === "check") {
@@ -14,4 +13,44 @@ list.addEventListener('click', (e) => {
   }
 });
 
+todoInput.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    if(input.value) {
+    createLi(input.value);
+    }
+    input.value = "";
+})
+
+function createLi(todo) {
+    //create a list item and give it classname todo
+    const li = document.createElement("LI");
+    li.className = "todo";
+    
+    //creating the button that will store the check mark image
+    const button = document.createElement("BUTTON");
+    button.className = "check";
+    const img = document.createElement("IMG");
+    img.setAttribute("src", "images/icon-check.svg");
+    button.appendChild(img);
+    //adding checkbox to list
+    li.appendChild(button)
+
+    //the p tag that will have the input value stored in it
+    const p = document.createElement("P");
+    p.textContent = todo;
+    //adding input value to list
+    li.appendChild(p);
+    
+    //button with cross picture in it
+    const cross = document.createElement("BUTTON");
+    cross.className = "cross";
+    const crossImg = document.createElement("IMG");
+    crossImg.setAttribute("src", "images/icon-cross.svg");
+    cross.appendChild(crossImg);
+    //adding cross to list item
+    li.appendChild(cross);
+    
+    //adding list item to the unordered list
+    list.appendChild(li);
+}
 
